bfcaf624dcd13c50de5941019d9fc3aeeb6b562e
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-07-01 17:41:00 -0600

    prueba final

--- test/integ.tes.js
+++ test/integ.tes.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../main');
 
-/* describe('Guardar prestamo', () => {
+describe('Guardar prestamo', () => {
     it('Deberia devolver el estado 200 en la ruta /addLend', async () => {
         const res = await request(app).post('/addLend').send({
             id: '981350720574685185',
@@ -19,16 +19,16 @@ const app = require('../main');
         expect(res.body.status).toEqual(200)
         console.log(res.body.result);
     });
-}) */
+})
 
-/* describe('Obtener prestamos por usuario', () => {
+describe('Obtener prestamos por usuario', () => {
     it('Deberia devolver el estado 200 en la ruta /getLends', async () => {
         const res = await request(app).get('/getLends/981350720574685185');
         expect(res.statusCode).toEqual(200);
         expect(res.body.status === 200 || res.body.status === 500).toBeTruthy()
         console.log(res.body);
     });
-}) */
+})
 
 describe('Obtener todos los prestamos', () => {
     it('Deberia devolver el estado 200 en la ruta /getAllLends', async () => {
--- test/unit.test.js
+++ test/unit.test.js
@@ -1,16 +1,16 @@
 const request = require('supertest');
 const app = require('../main');
 
-/* describe('API', () => {
+describe('API', () => {
     it('Debería devolver el estado 200 en la ruta raíz', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('info');
         console.log(res.body);
     });
-}); */
+});
 
-/* describe('Obtener usuarios', () => {
+describe('Obtener usuarios', () => {
     it('Debería devolver el estado 200 en la ruta /getUsers', async () => {
         const res = await request(app).get('/getUsers');
         expect(res.statusCode).toEqual(200);
@@ -18,7 +18,7 @@ const app = require('../main');
         expect(res.body.result).toBeInstanceOf(Array);
         console.log(res.body.result.length);
     });
-}) */
+})
 
 describe('Obtener insumos', () => {
     it('Debería devolver el estado 200 en la ruta /getInputs', async () => {
